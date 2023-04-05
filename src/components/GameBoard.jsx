62d15e5b7ece4f957126809bad945c2344ffa65a
Baekshook <ecgo0106@naver.com>
2023-04-05 11:36:45 +0900

    setting onClickCheck function

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const GameBoard = () => {
   const [hint, setHint] = useState("0 ~ 100 사이의 숫자를 맞춰보세요!");
-  const [randomNum, setRandomNum] = useState(Math.floor(Math.random() * 100)); // 랜덤함수 추출 및 Math.floor로 숫자 정수화
+  const [randomNum, setRandomNum] = useState(Math.floor(Math.random() * 101)); // 랜덤함수 추출 및 Math.floor로 숫자 정수화
   const [choiceNum, setChoiceNum] = useState("");
 
   const onChangeChoice = (e) => {
@@ -13,10 +13,33 @@ const GameBoard = () => {
     console.log(`랜덤 숫자는 ${randomNum}입니다.`);
   }, [randomNum]); // 의존성 배열 값 변할때 마다 useEffect 실행됨 (randomNum 추적하는 기능으로 쓰여짐)
 
-  useEffect(() => {
-    console.log(`유저가 선택한 숫자는 ${choiceNum}입니다.`);
-  }, [choiceNum]);
+  // useEffect(() => {
+  //   console.log(`유저가 선택한 숫자는 ${choiceNum}입니다.`);
+  // }, [choiceNum]);
 
+  const onClickCheck = () => {
+    // 1. 문자 입력시 setHint 변환
+    let checkNum = parseInt(choiceNum);
+    if (isNaN(checkNum)) {
+      //isNaN = Not a Number
+      setHint("숫자를 입력해주세요!");
+      return;
+    }
+    // 2. 0~100 이외의 숫자 입력 시 setHint 변환
+    if (0 > checkNum || checkNum > 100) {
+      setHint("숫자를 잘못 입력하셨습니다.");
+      return;
+    }
+
+    // 3. 랜덤 숫자와 유저가 선택한 숫자 비교
+    if (randomNum === checkNum) {
+      setHint("정답입니다!");
+    } else if (randomNum > checkNum) {
+      setHint(`정답은 ${checkNum}보다 높은 숫자입니다.`);
+    } else if (randomNum < checkNum) {
+      setHint(`정답은 ${checkNum}보다 낮은 숫자입니다.`);
+    }
+  };
 
   return (
     <div className=" w-full grow flex flex-col justify-center items-center">
@@ -28,7 +51,10 @@ const GameBoard = () => {
           value={choiceNum}
           onChange={onChangeChoice}
         />
-        <button className="px-4 py-2 ml-2 rounded-lg border-2 border-pink-300 text-pink-300 shadow-lg">
+        <button
+          onClick={onClickCheck}
+          className="px-4 py-2 ml-2 rounded-lg border-2 border-pink-300 text-pink-300 shadow-lg"
+        >
           확인
         </button>
       </div>
