1325254e1b4487ee3c907fd735d6f6b2116997ec
Baekshook <ecgo0106@naver.com>
2023-04-05 10:37:56 +0900

    useEffect로 randomNum 추적

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from "react";
+
 const GameBoard = () => {
+  const [hint, setHint] = useState("0 ~ 100 사이의 숫자를 맞춰보세요!");
+  const [randomNum, setRandomNum] = useState(Math.floor(Math.random() * 100)); // 랜덤함수 추출 및 Math.floor로 숫자 정수화 
+
+  useEffect(() => {
+    console.log(`랜덤 숫자는 ${randomNum}입니다.`)
+  },[randomNum]) // 의존성 배열 값 변할때 마다 useEffect 실행됨 (randomNum 추적하는 기능으로 쓰여짐)
+
   return (
     <div className=" w-full grow flex flex-col justify-center items-center">
-      <div className="mb-4 text-xl font-bold">
-        0 ~ 100 사이의 숫자를 맞춰보세요!
-      </div>
+      <div className="mb-4 text-xl font-bold">{hint}</div>
       <div>
         <input
           className="border-2 rounded-lg px-4 py-2 focus:outline-pink-300 shadow-lg"
