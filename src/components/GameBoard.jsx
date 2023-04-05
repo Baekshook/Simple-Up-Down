678ac00a1951c5b475d02f4813b932da091083be
Baekshook <ecgo0106@naver.com>
2023-04-05 11:48:28 +0900

    정답 시 랜덤값 초기화 기능

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -33,11 +33,12 @@ const GameBoard = () => {
 
     // 3. 랜덤 숫자와 유저가 선택한 숫자 비교
     if (randomNum === checkNum) {
-      setHint("정답입니다!");
+      setHint("정답입니다! 랜덤 값을 초기화 합니다.");
+      setRandomNum(Math.floor(Math.random() * 101));
     } else if (randomNum > checkNum) {
       setHint(`정답은 ${checkNum}보다 높은 숫자입니다.`);
     } else if (randomNum < checkNum) {
-      setHint(`정답은 ${checkNum}보다 낮은 숫자입니다.`);
+      setHint("정답은 " + checkNum + "보다 낮은 숫자입니다."); // `` 쓰지 않고 사용하는 방법
     }
   };
 
